577db3f3507c20c10b262b3f05264dea3162623d
Marcos Martín Vedriel <marcos.marved@gmail.com>
2015-07-25 20:34:24 +0200

    Added login Form in template

--- erasmus/erasmus/urls.py
+++ erasmus/erasmus/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from shareErasmus.views import (Home)
+from shareErasmus.views import (HomeView, LoginView, ContactView)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', Home.as_view()),
+    url(r'^$', HomeView.as_view()),
+    url(r'^login$', LoginView.as_view()),
+    url(r'^contacto$', ContactView.as_view()),
 ]
