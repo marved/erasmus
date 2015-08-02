30aba03e1b0d3e2dd496ef7aeb032d747b9fffdc
Marcos Martin <marcos_mv_9@hotmail.com>
2015-08-02 13:38:36 +0200

    Fix templates dirs error

--- erasmus/settings.py
+++ erasmus/settings.py
@@ -15,7 +15,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'erasmus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['shareErasmus/templates/shareErasmus'],
+        'DIRS': [os.path.join(BASE_DIR, 'shareErasmus/templates/shareErasmus/') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
