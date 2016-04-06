633af30c5be1bf1b29986c7939c9d59f8b00d880
Marcos Martin <marcos_mv_9@hotmail.com>
2016-04-06 15:21:28 +0200

    Created country filter in myUniversities form

--- shareErasmus/static/shareErasmus/js/controllers/settingsCtrl.js
+++ shareErasmus/static/shareErasmus/js/controllers/settingsCtrl.js
@@ -1,22 +1,34 @@
 app.controller('SettingsCtrl', ['$scope', 'shareErasmusApi', function ($scope, shareErasmusApi){
 
     $scope.isCollapsed = true;
+    $scope.countries = [];
+    $scope.countrySelected = null;
     $scope.universities = [];
+    $scope.filterUniversities = [];
     $scope.universitySelected = null;
     $scope.subjects = [];
     $scope.filterSubjects = [];
     $scope.subjectsSelected = [];
 
+
     shareErasmusApi.getUniversities().then(function (response) {
         $scope.universities = response.data;
+        loadCountries();
     });
 
     shareErasmusApi.getSubjects().then(function (response) {
         $scope.subjects = response.data;
     });
 
+    var loadCountries = function() {
+        for (var i=0; i<$scope.universities.length; i++) {
+            if (!~$scope.countries.indexOf($scope.universities[i].country)) {
+                $scope.countries.push($scope.universities[i].country);
+            }
+        }
+    };
 
-     var reloadFilterSubjects = function() {
+    var reloadFilterSubjects = function() {
         if ($scope.universitySelected == null || $scope.universitySelected == undefined) {
             console.log($scope.universitySelected);
             $scope.filterSubjects = $scope.subjects;
@@ -29,10 +41,29 @@ app.controller('SettingsCtrl', ['$scope', 'shareErasmusApi', function ($scope, s
                 $scope.filterSubjects.push($scope.subjects[i]);
             }
         }
-         console.log($scope.filterSubjects);
+        console.log($scope.countrySelected);
+    };
+
+    var reloadFilterUniversities = function() {
+        if ($scope.countrySelected == null || $scope.countrySelected == undefined) {
+            $scope.filterUniversities = $scope.universities;
+            return;
+        }
+
+        $scope.filterUniversities = [];
+        for (var i=0; i<$scope.universities.length; i++) {
+            if ($scope.universities[i].country == $scope.countrySelected){
+                $scope.filterUniversities.push($scope.universities[i]);
+            }
+        }
     };
 
     $scope.changeSelectedUniversity = function() {
         reloadFilterSubjects();
     };
+
+    $scope.changeSelectedCountry = function() {
+        reloadFilterUniversities();
+    };
+
 }]);
