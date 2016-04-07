b608851649fa5481259af497f54eb494f125634f
Marcos Martin <marcos_mv_9@hotmail.com>
2016-04-07 17:00:34 +0200

    Updated UserProfile model

--- shareErasmus/admin.py
+++ shareErasmus/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 
-from models import Link
 from models import UserProfile
 from models import University
 from models import Subject
 from models import Comment
 
-admin.site.register(Link)
 admin.site.register(UserProfile)
 admin.site.register(University)
 admin.site.register(Subject)
