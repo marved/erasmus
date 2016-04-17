7c68ca6939e1cacbf9382795a34c3825620cb3d9
Marcos Martin <marcos_mv_9@hotmail.com>
2016-04-17 19:09:41 +0200

    Added Country and City models

--- shareErasmus/admin.py
+++ shareErasmus/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
+from models import Country
+from models import City
 from models import UserProfile
 from models import University
 from models import Subject
 from models import Comment
 
+admin.site.register(Country)
+admin.site.register(City)
 admin.site.register(UserProfile)
 admin.site.register(University)
 admin.site.register(Subject)
