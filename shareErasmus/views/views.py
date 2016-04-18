f4ad4198ed0b75baa84fd1c09c5f24d1ae3df9d5
Marcos Martin <marcos_mv_9@hotmail.com>
2016-04-18 15:08:47 +0200

    Created detail-subject view

--- shareErasmus/views/views.py
+++ shareErasmus/views/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
-from shareErasmus.models import University
+from shareErasmus.models import University, Subject
 
 
 class HomeView(View):
@@ -29,10 +29,36 @@ class UniversityDetailView(View):
         if university_id:
             university = University.objects.get(pk=university_id)
 
+        subjects = Subject.objects.all().filter(university=university_id)
+
         context = {
-            'university': university
+            'university': university,
+            'subjects': subjects
         }
         return render(request, "pages/university-detail.html", context)
+
+
+class SubjectDetailView(View):
+    def get(self, request, **kwargs):
+
+        subject_id = kwargs.get("id_subject", None)
+        university_id = kwargs.get("id_university", None)
+        if subject_id and university_id:
+            try:
+                subject = Subject.objects.get(pk=subject_id)
+
+                if subject.university.pk == int(university_id):
+                    context = {
+                        'subject': subject
+                    }
+                    return render(request, "pages/subject-detail.html", context)
+            except:
+                pass
+
+        return render(request, "404.html")
+
+
+
 
 
 class ContactView(View):
