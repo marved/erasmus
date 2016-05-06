4f80d0175147e6a8cf150ae35d3b9eee92390a15
Marcos Martin <marcos_mv_9@hotmail.com>
2016-05-06 19:22:14 +0200

    Minor fix to restore filter

--- shareErasmus/static/shareErasmus/js/controllers/universitiesCtrl.js
+++ shareErasmus/static/shareErasmus/js/controllers/universitiesCtrl.js
@@ -1,9 +1,11 @@
 app.controller('UniversitiesCtrl', ['$scope', 'shareErasmusApi', function ($scope, shareErasmusApi){
 
     $scope.isCollapsed = true;
+    $scope.withFilter = false;
     $scope.universities = [];
     $scope.selectedFilterObject = null;
     $scope.selectedFilterUniversity = null;
+
     $scope.$watch("selectedFilterObject",function(newValue,oldValue) {
         if (newValue != null) {
             $scope.selectedFilterUniversity = newValue.originalObject
@@ -11,6 +13,7 @@ app.controller('UniversitiesCtrl', ['$scope', 'shareErasmusApi', function ($scop
                                                 $scope.selectedFilterUniversity.name).then(function (response) {
                 $scope.universities = [];
                 $scope.universities.push(response.data);
+                $scope.withFilter = true;
             });
           return;
         }
@@ -21,6 +24,7 @@ app.controller('UniversitiesCtrl', ['$scope', 'shareErasmusApi', function ($scop
     });
 
     $scope.restoreFilter = function() {
+        $scope.withFilter = false;
         shareErasmusApi.getUniversities().then(function (response) {
             $scope.universities = response.data;
         });
